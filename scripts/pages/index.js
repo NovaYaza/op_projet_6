a3fb547606048f86a398c78d8deaba7e674c0c66
NovaYaza <sefa151000@gmail.com>
2024-05-31 10:43:07 +0200

    affichage des photographes et de leurs infos sur l'index.html

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,17 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        const response = await fetch('data/photographers.json');
+        // Vérifiez si la requête a réussi
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);// Changer pour un message erreur côté utilisateur
+        }
+        // Convertion de la réponse en JSON
+        const data = await response.json();
+        console.log(data);
+        // Retourner les données des photographes
+        return {
+            photographers: data.photographers
+        };
+
     }
 
     async function displayData(photographers) {
@@ -40,6 +28,8 @@
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+
+        /* console.log(photographers);  */
     }
     
     init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,22 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+
+        const taglineElement = document.createElement('p');
+        taglineElement.textContent = tagline;
+
+        const priceElement = document.createElement('p');
+        priceElement.textContent = `${price}€/jour`;
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
         return (article);
     }
     return { name, picture, getUserCardDOM }
